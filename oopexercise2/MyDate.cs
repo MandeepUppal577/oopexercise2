d3b84eeafcf06bcbc036177c5aab8dba31c6e773
Mandeep kaur <mandeepuppal131@gmail.com>
2017-07-15 00:41:28 -0400

    first commit

--- oopexercise2/MyDate.cs
+++ oopexercise2/MyDate.cs
@@ -8,6 +8,15 @@ namespace oopexercise2
 {
     class MyDate
     {
-        
+        private int year = 1;
+        private int month = 1;
+        private int day = 1;
+        private string[] strMonths = { "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+                                       "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
+        private string[] strDays = { "Sunday", "Monday", "Tuesday", "Wednesday"
+                                       , "Thursday", "Friday", "Saturday"};
+
+        private int[] daysInMonths = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
     }
 }
